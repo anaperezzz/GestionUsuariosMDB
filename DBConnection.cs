4db5fc32d7642b13c4c4ccff482438847cf7e0de
anaperezzz <anamariapc060397@gmail.com>
2026-09-03 21:41:15 -0600

    Agregar archivos de proyecto.

---
+++ DBConnection.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Data.SqlClient;
+
+namespace GestionUsuarios
+{
+    internal class DBConnection
+    {
+        private string ConnectionString = "Data Source=3375898HP;Initial Catalog=GestionUsuarios;User ID=sa;Password=123456;Encrypt=True;TrustServerCertificate=True;";
+
+        public SqlConnection ObtenerConexion()
+        {
+            SqlConnection connection = new SqlConnection(ConnectionString);
+            return connection;
+        }
+    }
+}
---
+++ UsuarioDto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Data.SqlClient;
+
+namespace GestionUsuarios
+{
+    internal class UsuarioDto
+    {
+        private int id;
+        private string nombre;
+        private string apellido;
+        private string email;
+        private string pais;
+
+        public int Id { get => id; set => id = value; }
+        public string Nombre { get => nombre; set => nombre = value; }
+        public string Apellido { get => apellido; set => apellido = value; }
+        public string Email { get => email; set => email = value; }
+        public string Pais { get => pais; set => pais = value; }
+    }
+}
